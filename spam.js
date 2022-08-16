42c2e33dd830e0420702af493b9b3d1a2849c080
Hardwaregore <hardwaregore101@gmail.com>
2022-08-16 13:24:26 -0500

    Added node packages

--- spam.js
+++ spam.js
@@ -1,8 +1,35 @@
 #!/usr/bin/node
 
-const fs = require('fs');
+import fs from 'fs'
 
 // Read the api key from api.pem
 var apiKey = fs.readFileSync('api.pem');
 
-console.log('apiKey: ' + apiKey);
+var pid = fs.readFileSync('pid.pem');
+
+var number = fs.readFileSync('number');
+
+console.log('API Key: ' + apiKey);
+console.log('Project ID: ' + pid);
+console.log('Number: ' + number);
+
+import { Voice } from '@signalwire/realtime-api'
+
+const client = new Voice.Client({
+  project: pid,
+  token: apiKey,
+  contexts: ["SPAM"]
+})
+
+try {
+  const call = await client.dialPhone({
+    from: number,  // The number you bought from SignalWire
+    to: number,
+    timeout: 30
+  })
+
+  console.log('The call has been answered!', call.id)
+} catch (e) {
+  console.error(e)
+}
+
